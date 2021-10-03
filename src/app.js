620f42661f635769b70f442619c2459c5729ee8a
Daniel Leon <dafeleon2699@gmail.com>
2021-10-02 19:12:38 -0500

    appClaves

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const express = require("express"); //llamar al express
 const app = express();
 
 const PORT = 443;
+
 const public = path.join(__dirname, "../public");
 const views = path.join(__dirname, "../templates/views");
 const partials = path.join(__dirname, "../templates/partials");
@@ -12,19 +13,19 @@ const partials = path.join(__dirname, "../templates/partials");
 // SetUp directory to serve
 app.use(express.static(public));
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.status(200);
   res.sendFile("index.html", { root: public });
   flag_log = 0; // para que al darle salir no vuelva a entrar
 });
 
-app.get("/contact", function (req, res) {
+app.get("/contact", (req, res) => {
   res.status(200);
   res.sendFile("contactus.html", { root: public });
   flag_log = 0; // para que al darle salir no vuelva a entrar
 });
 
-app.get("/login", function (req, res) {
+app.get("/login", (req, res) => {
   res.status(200);
   res.sendFile("login.html", { root: public });
   flag_log = 0; // para que al darle salir no vuelva a entrar
@@ -42,6 +43,6 @@ https
     },
     app
   )
-  .listen(PORT, function () {
+  .listen(PORT, () => {
     console.log("My HTTPS server listening on port " + PORT + "...");
   });
