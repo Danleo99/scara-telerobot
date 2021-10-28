86160d5d0cb3caad48969dd11a04d9399df7c02a
Danleo99 <dafeleon2699@gmail.com>
2021-10-28 08:39:39 -0500

    Initial Server

--- server/src/test.js
+++ server/src/test.js
@@ -1,4 +1,28 @@
 const path = require("path");
-const keys  =  path.join(__dirname,"../../certificates")
+const express = require("express"); //llamar al express
+const app = express();
 
-console.log(`${keys}/PubKey.pem`)
+const public = path.join(__dirname, "../public");
+
+// SetUp directory to serve
+app.use(express.static(public));
+
+app.get("/", function (req, res) {
+  res.status(200);
+  res.sendFile("index.html", { root: public });
+  flag_log = 0; // para que al darle salir no vuelva a entrar
+});
+app.get("/contact", function (req, res) {
+  res.status(200);
+  res.sendFile("contactus.html", { root: public });
+  flag_log = 0; // para que al darle salir no vuelva a entrar
+});
+app.get("/login", function (req, res) {
+  res.status(200);
+  res.sendFile("login.html", { root: public });
+  flag_log = 0; // para que al darle salir no vuelva a entrar
+});
+
+app.listen(3000, '0.0.0.0', function() {
+  console.log('Listening to port:  ' + 3000);
+});
