9b7f1125fea6d62a5015a6c50105d4622a635527
Daniel Leon <82685546+Danleo99@users.noreply.github.com>
2022-02-12 00:57:05 -0500

    signin and login completed

--- server/db/dbconnect.js
+++ server/db/dbconnect.js
@@ -1,19 +1,30 @@
 const mysql = require('mysql2')
 
-const connection = mysql.createPool({
+const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: 'Tesis2022!',
     database: 'telerobot'
 });
 
-connection.getConnection((e) => {
-    if (e) {
-        console.log(e);
-        return;
+pool.getConnection((err, connection) => {
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.log('Connection to MySQL lost');
+        } else if (err.code === 'ER_CON_COUNT_ERROR') {
+            console.log('Database has to many connections');
+        } else if (err.code === 'ECONNREFUSED') {
+            console.log('Database connection refused');
+        } else {
+            console.log(err);
+            return;
+        }
     } else {
-        console.log('Database connected')
+        connection.release();
+        console.log('Database connected');
     }
 });
 
-module.exports = connection;
+const promisePool = pool.promise();
+
+module.exports = promisePool;
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,46 +1,46 @@
-const express = require('express')
-const database = require('../../db/dbconnect')
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs')
+const express = require('express');
+const database = require('../../db/dbconnect');
+const { secureUser } = require('../../middleware/auth');
+const bcrypt = require('bcryptjs');
 const router = new express.Router()
 
-const tokenKey = 'Secure@telerobot2022'
-
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     const { email, password } = req.body
     const query1 = 'SELECT * FROM users WHERE email LIKE ? LIMIT 1'
-    database.query(query1, [email],
-        (e, rows, fields) => {
-            if (rows.length == 0) {
-                const token = jwt.sign({ password }, tokenKey)
-                const salt = bcrypt.genSaltSync(8);
-                const hashPassword = bcrypt.hashSync(password, salt);
-
-                const query2 = 'INSERT INTO users(email, password, token) VALUES (?, ?, ?)'
-                database.query(query2, [email, hashPassword, token],
-                    (err, rows, fields) => {
-                        if (!err) {
-                            res.status(200).send()
-                        } else {
-                            res.status(400).send(err)
-                        }
-                    });
-            } else {
-                res.status(400).send('Este correo ya esta registrado')
-            };
-        });
+    const query2 = 'INSERT INTO users(email, password, token) VALUES (?, ?, ?)'
+    try {
+        const [rows, fields] = await database.query(query1, [email]);
+        if (rows.length == 0) {
+            const [token, hashPassword] = await secureUser(password);
+            const [rows, fields] = await database.query(query2, [email, hashPassword, token]);
+            res.status(201).send(token);
+        } else {
+            res.status(409).send('Este correo ya esta registrado')
+        };
+    } catch (error) {
+        res.status(400).send(error)
+    }
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
     const { email, password } = req.body
-    const query = 'SELECT * FROM users WHERE email LIKE ? AND password LIKE ?';
-    database.query(query, [email, password], (e, rows, fields) => {
-        if (!e) {
-            res.json(rows[0]);
+    const query = 'SELECT * FROM users WHERE email LIKE ?';
+    try {
+        const [rows, fields] = await database.query(query, [email]);
+        if (rows.length != 0) {
+            const approved = await bcrypt.compare(password, rows[0].password)
+            if (approved) {
+                res.status(202).send(rows[0].token)
+            } else {
+                res.status(401).send()
+            }
+
         } else {
-            console.log(e);
+            res.status(406).send('El usuario no esta registrado')
         }
-    });
+    } catch (error) {
+        res.status(400).send(error)
+    }
 });
 
 module.exports = router;
