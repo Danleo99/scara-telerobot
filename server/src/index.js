1cb743c06990b1a912a23c9f8a90c08226a9628e
Daniel Leon <82685546+Danleo99@users.noreply.github.com>
2022-03-16 22:22:44 -0500

    server in order

--- server/src/index.js
+++ server/src/index.js
@@ -9,10 +9,11 @@ const port = process.env.PORT || 80;
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public_html')));
+app.use(express.static(path.join(__dirname, '../public_html')));
 app.use('/user', userRoutes);
 app.use('/routines', routinesRouter);
 
 app.listen(port, '0.0.0.0', () => {
+  console.log(path.join(__dirname, '../public_html'))
   console.log('Listening to port: ' + port);
 });
